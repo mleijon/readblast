9511543c0a200116e4b7deb1e4053ab087be5d99
mleijon <mikael.leijon@gmail.com>
2019-02-14 16:25:42 +0100

    testar skicka readblast

---
+++ merge_paired_reads.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+#for f in $dirs;do cd $f;./*.sh; cd ../..;done
+
+FILES="/home/decypher/micke/data/clinf/reindeer/181214/*R1.fastq.gz"
+for f in $FILES; do
+cat ${f/R1/R2} >> $f; wait
+cat ${f/R1/SE} >> $f; wait
+rm ${f/R1/R2}; wait
+rm ${f/R1/SE}; wait
+gunzip $f; wait
+rename _R1.fastq .fastq ${f/.gz/}; wait
+done
